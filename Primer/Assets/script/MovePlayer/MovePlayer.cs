85c5e312a53f975d720466877c892690b574a1a8
Lucas <montobybianchi@gmail.com>
2023-01-24 19:00:08 -0300

    Scena rotations

---
+++ Primer/Assets/script/MovePlayer/MovePlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    public float rotationSpeed = 90f;
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(horizontal, 0, vertical);
+        MoveDirection(direction);
+        CharacterDirection();
+    }
+
+    private void MoveDirection(Vector3 moveDirection)
+    {
+        transform.position += moveDirection * (speed * Time.deltaTime);
+    }
+
+    
+    private void CharacterDirection()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
+    }
+
+}
