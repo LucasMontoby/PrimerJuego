7d691e3194e9799e33f9c46595a6b174e26ad2fe
Lucas <montobybianchi@gmail.com>
2023-01-25 20:26:56 -0300

    Vectores MovePlay and Enemy

---
+++ Primer/Assets/script/MovePlayer-Enemy/Enemy.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float speedToLook;
+    [SerializeField] float speed;
+    [SerializeField] float persuitDistance;
+    [SerializeField] private enemyState currentEstate;
+
+    public enum enemyState
+    {
+        LookAtPlayer,
+        MoveEnemy2, 
+        fullEnemy
+    }
+
+    void Update()
+    {
+        switch (currentEstate)
+        {
+            case enemyState.LookAtPlayer:
+            LookAtPlayer();
+            break;  
+            case enemyState.MoveEnemy2: 
+            MoveEnemy2();   
+            break;
+            case enemyState.fullEnemy:
+            LookAtPlayer();
+            MoveEnemy2();
+            break;  
+
+        }
+    }
+
+    private void LookAtPlayer()
+    {
+        Quaternion newRotations = Quaternion.LookRotation(playerTransform.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotations, speedToLook * Time.deltaTime);
+    }
+
+    private void MoveEnemy2()
+    {
+        var vectorPlayer = playerTransform.position - transform.position;
+        var distance = vectorPlayer.magnitude;
+
+        if (distance > persuitDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+               playerTransform.position, Time.deltaTime * speed);
+        }
+    }
+}
--- Primer/Assets/script/MovePlayer-Enemy/MovePlayer.cs
+++ Primer/Assets/script/MovePlayer-Enemy/MovePlayer.cs
@@ -6,25 +6,21 @@ public class MovePlayer : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed;
+
     private void Update()
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
         var direction = new Vector3(horizontal, 0, vertical);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
         MoveDirection(direction);
-        CharacterDirection();
     }
 
     private void MoveDirection(Vector3 moveDirection)
     {
         transform.position += moveDirection * (speed * Time.deltaTime);
-    }
-
-    
-    private void CharacterDirection()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
     }
 
 }
