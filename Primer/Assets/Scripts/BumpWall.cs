35fcdd292ebbeaa9f2317e276d749ef29a03fa31
Lucas <montobybianchi@gmail.com>
2023-01-26 19:39:22 -0300

    Collaiders

---
+++ Primer/Assets/Scripts/BumpWall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpWall : MonoBehaviour
+{
+    [SerializeField] private Transform[] newPositions;
+
+    public Transform GetPosition()
+    {
+        return newPositions[Random.Range(0, newPositions.Length)];
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Collision");
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision");
+    }
+
+}
+
+
